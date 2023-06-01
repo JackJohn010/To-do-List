42a46a3e21412861520e132058039a8c04e9810b
Johnny <johnnysilva991@gmail.com>
2023-06-01 12:16:38 -0300

    remoção da bordar e adicionado cor de fundo

--- src/components/AddTask/index.jsx
+++ src/components/AddTask/index.jsx
@@ -5,13 +5,13 @@ const AddTask = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("Tarefa Criada!!")
+        alert("Tarefa Criada!! :)")
     }
 
     return (
         <div className={styles.container}>
             <div className={styles.boderForm}>
-                <h1>LISTA DE TAREFAS</h1>
+                <h1>Lista de Tarefa</h1>
                 <form onSubmit={handleSubmit}>
                         <input type="text" placeholder='Digite a tarefa' className={styles.input_style}/>
                         <input type="submit" value="Adicionar tarefa" className={styles.style__submit} />
